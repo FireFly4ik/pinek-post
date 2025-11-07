ae77e0d5ccc4e618d26af37995405b15514daac1
FireFly4ik <andreybatman06@gmail.com>
2025-11-07 19:44:53 +0300

    feat: created db methods and grpc methods

--- internal/db/post.go
+++ internal/db/post.go
@@ -1,5 +1,676 @@
 package db
 
-func (d *AuthDatabase) AutoMigrate() error {
+import (
+	"errors"
+	"github.com/google/uuid"
+	"strings"
+)
 
+var (
+	ErrCannotParseUUID    = errors.New("cannot parse uuid")
+	ErrPostNotFound       = errors.New("post not found")
+	ErrBoardNotFound      = errors.New("board not found")
+	ErrTagAlreadyExists   = errors.New("tag already exists")
+	ErrTagNotFound        = errors.New("tag not found")
+	ErrModelAlreadyExists = errors.New("model already exists")
+)
+
+func (d *PostDatabase) CreatePost(postId, userId, title string, description *string) (string, error) {
+	postIdSplitted := strings.Split(postId, ".")
+
+	postIdParsed, err := uuid.Parse(postIdSplitted[0])
+	if err != nil {
+		return "", ErrCannotParseUUID
+	}
+
+	userIdParsed, err := uuid.Parse(userId)
+	if err != nil {
+		return "", ErrCannotParseUUID
+	}
+
+	post := &Post{
+		ID:          postIdParsed,
+		UserID:      userIdParsed,
+		Title:       title,
+		Description: description,
+		Extension:   postIdSplitted[1],
+	}
+
+	tx := d.Database.Begin()
+
+	if err := tx.Create(post).Error; err != nil {
+		tx.Rollback()
+		return "", err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return "", err
+	}
+
+	return postId, nil
+}
+
+func (d *PostDatabase) UpdatePost(postId, userId string, title, description *string) error {
+	postIdParsed, err := uuid.Parse(postId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	userIdParsed, err := uuid.Parse(userId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	tx := d.Database.Begin()
+
+	post := &Post{}
+
+	if err := tx.Where("id = ? AND user_id = ?", postIdParsed, userIdParsed).First(post).Error; err != nil {
+		tx.Rollback()
+		return errors.Join(ErrPostNotFound, err)
+	}
+
+	if title != nil {
+		post.Title = *title
+	}
+
+	if description != nil {
+		post.Description = description
+	}
+
+	if err := tx.Save(post).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+func (d *PostDatabase) GetPost(postId string) (string, string, string, *string, string, []string, error) {
+	postIdParsed, err := uuid.Parse(postId)
+	if err != nil {
+		return "", "", "", nil, "", nil, ErrCannotParseUUID
+	}
+
+	post := &Post{}
+	if err := d.Database.Where("id = ?", postIdParsed).First(post).Error; err != nil {
+		return "", "", "", nil, "", nil, errors.Join(ErrPostNotFound, err)
+	}
+
+	tags := []Tag{}
+	if err := d.Database.Where("post_id = ?", postIdParsed).Find(&tags).Error; err != nil {
+		return "", "", "", nil, "", nil, err
+	}
+
+	tagsStrings := make([]string, len(tags))
+	for i, tag := range tags {
+		tagsStrings[i] = tag.ID.String()
+	}
+
+	return post.ID.String(), post.UserID.String(), post.Title, post.Description, post.Extension, tagsStrings, nil
+}
+
+func (d *PostDatabase) GetPosts(postIds []string) ([]string, []string, []string, []*string, []string, [][]string, error) {
+	userIds := make([]string, len(postIds))
+	titles := make([]string, len(postIds))
+	descriptions := make([]*string, len(postIds))
+	extensions := make([]string, len(postIds))
+	tags := make([][]string, len(postIds))
+
+	for i := range postIds {
+		postId, userId, title, description, extension, tag, err := d.GetPost(postIds[i])
+		if err != nil {
+			postIds = append(postIds, "")
+			userIds = append(userIds, "")
+			titles = append(titles, "")
+			descriptions = append(descriptions, nil)
+			extensions = append(extensions, "")
+			tags = append(tags, nil)
+		} else {
+			postIds = append(postIds, postId)
+			userIds = append(userIds, userId)
+			titles = append(titles, title)
+			descriptions = append(descriptions, description)
+			extensions = append(extensions, extension)
+			tags = append(tags, tag)
+		}
+	}
+
+	return postIds, userIds, titles, descriptions, extensions, tags, nil
+}
+
+func (d *PostDatabase) SearchPosts(query *string, userIdsSearch, tagIdsSearch []string, limit, offset int) ([]string, []string, []string, []*string, []string, [][]string, error) {
+	dbQuery := d.Database.Model(&Post{})
+
+	if query != nil {
+		dbQuery = dbQuery.Where("title ILIKE ?", "%"+*query+"%")
+	}
+
+	if userIdsSearch != nil {
+		userIdsParsed := []uuid.UUID{}
+		for _, userId := range userIdsSearch {
+			userIdParsed, err := uuid.Parse(userId)
+			if err != nil {
+				return nil, nil, nil, nil, nil, nil, ErrCannotParseUUID
+			}
+			userIdsParsed = append(userIdsParsed, userIdParsed)
+		}
+
+		dbQuery = dbQuery.Where("user_id IN ?", userIdsParsed)
+	}
+
+	if tagIdsSearch != nil {
+		tagIdsParsed := []uuid.UUID{}
+		for _, tagId := range tagIdsSearch {
+			tagIdParsed, err := uuid.Parse(tagId)
+			if err != nil {
+				return nil, nil, nil, nil, nil, nil, ErrCannotParseUUID
+			}
+			tagIdsParsed = append(tagIdsParsed, tagIdParsed)
+		}
+
+		dbQuery = dbQuery.Joins("JOIN post_tags ON posts.id = post_tags.post_id").Where("post_tags.tag_id IN ?", tagIdsParsed)
+	}
+
+	posts := []Post{}
+	if err := dbQuery.Limit(limit).Offset(offset).Find(&posts).Error; err != nil {
+		return nil, nil, nil, nil, nil, nil, err
+	}
+
+	postIds := make([]string, len(posts))
+	userIds := make([]string, len(posts))
+	titles := make([]string, len(posts))
+	descriptions := make([]*string, len(posts))
+	extensions := make([]string, len(posts))
+	tags := make([][]string, len(posts))
+
+	for i, post := range posts {
+		postIds[i] = post.ID.String()
+		userIds[i] = post.UserID.String()
+		titles[i] = post.Title
+		descriptions[i] = post.Description
+		extensions[i] = post.Extension
+
+		tagModels := []Tag{}
+		if err := d.Database.Where("post_id = ?", post.ID).Find(&tagModels).Error; err != nil {
+			return nil, nil, nil, nil, nil, nil, err
+		}
+
+		tagStrings := make([]string, len(tagModels))
+		for j, tag := range tagModels {
+			tagStrings[j] = tag.ID.String()
+		}
+
+		tags[i] = tagStrings
+	}
+
+	return postIds, userIds, titles, descriptions, extensions, tags, nil
+}
+
+func (d *PostDatabase) DeletePost(postId, userId string) error {
+	postIdParsed, err := uuid.Parse(postId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	userIdParsed, err := uuid.Parse(userId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	tx := d.Database.Begin()
+
+	if err := tx.Where("id = ? AND user_id = ?", postIdParsed, userIdParsed).Delete(&Post{}).Error; err != nil {
+		tx.Rollback()
+		return errors.Join(ErrPostNotFound, err)
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+func (d *PostDatabase) CreateBoard(userId, name string, description *string) (string, error) {
+	userIdParsed, err := uuid.Parse(userId)
+	if err != nil {
+		return "", ErrCannotParseUUID
+	}
+
+	board := &Board{
+		UserID:      userIdParsed,
+		Name:        name,
+		Description: description,
+	}
+
+	tx := d.Database.Begin()
+
+	if err := tx.Create(board).Error; err != nil {
+		tx.Rollback()
+		return "", err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return "", err
+	}
+
+	return board.ID.String(), nil
+}
+
+func (d *PostDatabase) UpdateBoard(boardId, userId string, name, description *string) error {
+	boardIdParsed, err := uuid.Parse(boardId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	userIdParsed, err := uuid.Parse(userId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	tx := d.Database.Begin()
+
+	board := &Board{}
+
+	if err := tx.Where("id = ? AND user_id = ?", boardIdParsed, userIdParsed).First(board).Error; err != nil {
+		tx.Rollback()
+		return errors.Join(ErrBoardNotFound, err)
+	}
+
+	if name != nil {
+		board.Name = *name
+	}
+
+	if description != nil {
+		board.Description = description
+	}
+
+	if err := tx.Save(board).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+func (d *PostDatabase) GetBoard(boardId string) (string, string, string, *string, []string, error) {
+	boardIdParsed, err := uuid.Parse(boardId)
+	if err != nil {
+		return "", "", "", nil, nil, ErrCannotParseUUID
+	}
+
+	board := &Board{}
+	if err := d.Database.Where("id = ?", boardIdParsed).First(board).Error; err != nil {
+		return "", "", "", nil, nil, errors.Join(ErrBoardNotFound, err)
+	}
+
+	posts := []Post{}
+	if err := d.Database.Where("board_id = ?", boardIdParsed).Find(&posts).Error; err != nil {
+		return "", "", "", nil, nil, err
+	}
+
+	postIds := make([]string, len(posts))
+	for i, post := range posts {
+		postIds[i] = post.ID.String()
+	}
+
+	return board.ID.String(), board.UserID.String(), board.Name, board.Description, postIds, nil
+}
+
+func (d *PostDatabase) GetBoards(boardIds []string) ([]string, []string, []string, []*string, [][]string, error) {
+	userIds := make([]string, len(boardIds))
+	names := make([]string, len(boardIds))
+	descriptions := make([]*string, len(boardIds))
+	postsIds := make([][]string, len(boardIds))
+
+	for i := range boardIds {
+		boardId, userId, name, description, postIds, err := d.GetBoard(boardIds[i])
+		if err != nil {
+			boardIds = append(boardIds, "")
+			userIds = append(userIds, "")
+			names = append(names, "")
+			descriptions = append(descriptions, nil)
+			postsIds = append(postsIds, nil)
+		} else {
+			boardIds = append(boardIds, boardId)
+			userIds = append(userIds, userId)
+			names = append(names, name)
+			descriptions = append(descriptions, description)
+			postsIds = append(postsIds, postIds)
+		}
+	}
+
+	return boardIds, userIds, names, descriptions, postsIds, nil
+}
+
+func (d *PostDatabase) SearchBoards(query *string, userId []string, limit, offset int) ([]string, []string, []string, []*string, [][]string, error) {
+	dbQuery := d.Database.Model(&Board{}).Joins("LEFT JOIN board_posts ON boards.id = board_posts.board_id")
+
+	if query != nil {
+		dbQuery = dbQuery.Where("name ILIKE ?", "%"+*query+"%")
+	}
+
+	if userId != nil {
+		userIdsParsed := []uuid.UUID{}
+		for _, uId := range userId {
+			userIdParsed, err := uuid.Parse(uId)
+			if err != nil {
+				return nil, nil, nil, nil, nil, ErrCannotParseUUID
+			}
+			userIdsParsed = append(userIdsParsed, userIdParsed)
+		}
+
+		dbQuery = dbQuery.Where("user_id IN ?", userIdsParsed)
+	}
+
+	boards := []Board{}
+	if err := dbQuery.Limit(limit).Offset(offset).Find(&boards).Error; err != nil {
+		return nil, nil, nil, nil, nil, err
+	}
+
+	boardIds := make([]string, len(boards))
+	userIds := make([]string, len(boards))
+	names := make([]string, len(boards))
+	descriptions := make([]*string, len(boards))
+	postsIds := make([][]string, len(boards))
+
+	for i, board := range boards {
+		boardIds[i] = board.ID.String()
+		userIds[i] = board.UserID.String()
+		names[i] = board.Name
+		descriptions[i] = board.Description
+		postModels := []Post{}
+		if err := d.Database.Where("board_id = ?", board.ID).Find(&postModels).Error; err != nil {
+			return nil, nil, nil, nil, nil, err
+		}
+
+		postStrings := make([]string, len(postModels))
+		for j, post := range postModels {
+			postStrings[j] = post.ID.String()
+		}
+
+		postsIds[i] = postStrings
+	}
+
+	return boardIds, userIds, names, descriptions, postsIds, nil
+}
+
+func (d *PostDatabase) DeleteBoard(boardId, userId string) error {
+	boardIdParsed, err := uuid.Parse(boardId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	userIdParsed, err := uuid.Parse(userId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	tx := d.Database.Begin()
+
+	if err := tx.Where("id = ? AND user_id = ?", boardIdParsed, userIdParsed).Delete(&Board{}).Error; err != nil {
+		tx.Rollback()
+		return errors.Join(ErrBoardNotFound, err)
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+func (d *PostDatabase) CreateTag(name string) (string, error) {
+	tag := &Tag{
+		Name: name,
+	}
+
+	tx := d.Database.Begin()
+
+	alreadyExistsTag := &Tag{}
+	if err := tx.Where("name = ?", name).First(alreadyExistsTag).Error; err == nil {
+		tx.Rollback()
+		return "", err
+	}
+
+	if alreadyExistsTag.ID != uuid.Nil {
+		tx.Rollback()
+		return "", ErrTagAlreadyExists
+	}
+
+	if err := tx.Create(tag).Error; err != nil {
+		tx.Rollback()
+		return "", err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return "", err
+	}
+
+	return tag.ID.String(), nil
+}
+
+func (d *PostDatabase) GetTag(tagId string) (string, string, error) {
+	tagIdParsed, err := uuid.Parse(tagId)
+	if err != nil {
+		return "", "", ErrCannotParseUUID
+	}
+
+	tag := &Tag{}
+	if err := d.Database.Where("id = ?", tagIdParsed).First(tag).Error; err != nil {
+		return "", "", errors.Join(ErrTagNotFound, err)
+	}
+
+	return tag.ID.String(), tag.Name, nil
+}
+
+func (d *PostDatabase) SearchTags(query *string, limit, offset int) ([]string, []string, error) {
+	dbQuery := d.Database.Model(&Tag{})
+
+	if query != nil {
+		dbQuery = dbQuery.Where("name ILIKE ?", "%"+*query+"%")
+	}
+
+	tags := []Tag{}
+	if err := dbQuery.Limit(limit).Offset(offset).Find(&tags).Error; err != nil {
+		return nil, nil, err
+	}
+
+	tagIds := make([]string, len(tags))
+	names := make([]string, len(tags))
+
+	for i, tag := range tags {
+		tagIds[i] = tag.ID.String()
+		names[i] = tag.Name
+	}
+
+	return tagIds, names, nil
+}
+
+func (d *PostDatabase) PinPostToBoard(postId, boardId, userId string) error {
+	postIdParsed, err := uuid.Parse(postId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	boardIdParsed, err := uuid.Parse(boardId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	userIdParsed, err := uuid.Parse(userId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	board := &Board{}
+	if err := d.Database.Where("id = ? AND user_id = ?", boardIdParsed, userIdParsed).First(board).Error; err != nil {
+		return errors.Join(ErrBoardNotFound, err)
+	}
+
+	boardPost := &BoardPost{
+		BoardID: boardIdParsed,
+		PostID:  postIdParsed,
+	}
+
+	alreadyExistsModel := &BoardPost{}
+	if err := d.Database.Where("board_id = ? AND post_id = ?", boardIdParsed, postIdParsed).First(alreadyExistsModel).Error; err == nil {
+		return nil
+	}
+
+	if alreadyExistsModel.BoardID != uuid.Nil && alreadyExistsModel.PostID != uuid.Nil {
+		return ErrModelAlreadyExists
+	}
+
+	tx := d.Database.Begin()
+
+	if err := tx.Create(boardPost).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+func (d *PostDatabase) UnpinPostFromBoard(postId, boardId, userId string) error {
+	postIdParsed, err := uuid.Parse(postId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	boardIdParsed, err := uuid.Parse(boardId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	userIdParsed, err := uuid.Parse(userId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	tx := d.Database.Begin()
+
+	if err := tx.Joins("JOIN boards ON board_posts.board_id = boards.id").Error; err != nil {
+		tx.Rollback()
+		return errors.Join(ErrBoardNotFound, err)
+	}
+
+	if err := tx.Where("board_id = ? AND post_id = ? AND boards.user_id = ?", boardIdParsed, postIdParsed, userIdParsed).Delete(&BoardPost{}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+func (d *PostDatabase) AddTagToPost(postId, tagId, userId string) error {
+	postIdParsed, err := uuid.Parse(postId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	tagIdParsed, err := uuid.Parse(tagId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	userIdParsed, err := uuid.Parse(userId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	post := &Post{}
+	if err := d.Database.Where("id = ? AND user_id = ?", postIdParsed, userIdParsed).First(post).Error; err != nil {
+		return errors.Join(ErrPostNotFound, err)
+	}
+
+	postTag := &PostTag{
+		PostID: postIdParsed,
+		TagID:  tagIdParsed,
+	}
+
+	alreadyExistsModel := &PostTag{}
+	if err := d.Database.Where("post_id = ? AND tag_id = ?", postIdParsed, tagIdParsed).First(alreadyExistsModel).Error; err == nil {
+		return nil
+	}
+
+	if alreadyExistsModel.PostID != uuid.Nil && alreadyExistsModel.TagID != uuid.Nil {
+		return ErrModelAlreadyExists
+	}
+
+	tx := d.Database.Begin()
+
+	if err := tx.Create(postTag).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+func (d *PostDatabase) RemoveTagFromPost(postId, tagId, userId string) error {
+	postIdParsed, err := uuid.Parse(postId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	tagIdParsed, err := uuid.Parse(tagId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	userIdParsed, err := uuid.Parse(userId)
+	if err != nil {
+		return ErrCannotParseUUID
+	}
+
+	post := &Post{}
+	if err := d.Database.Where("id = ? AND user_id = ?", postIdParsed, userIdParsed).First(post).Error; err != nil {
+		return errors.Join(ErrPostNotFound, err)
+	}
+
+	tx := d.Database.Begin()
+
+	if err := tx.Where("post_id = ? AND tag_id = ?", postIdParsed, tagIdParsed).Delete(&PostTag{}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
 }
--- internal/grpc/grpc.go
+++ internal/grpc/grpc.go
@@ -1,6 +1,7 @@
 package grpc
 
 import (
+	"context"
 	"post/internal/config"
 	"post/internal/db"
 	pb "post/internal/proto"
@@ -17,4 +18,333 @@ func NewPostServer(db *db.PostDatabase, cfg *config.Config) *PostServiceServer {
 		database: db,
 		envConf:  cfg,
 	}
+}
+
+func (p *PostServiceServer) CreatePost(ctx context.Context, req *pb.CreatePostRequest) (*pb.CreatePostResponse, error) {
+	var description *string
+	if req.Description != "" {
+		description = &req.Description
+	}
+
+	postId, err := p.database.CreatePost(req.PostId, req.UserId, req.Title, description)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CreatePostResponse{
+		PostId: postId,
+	}, nil
+}
+
+func (p *PostServiceServer) UpdatePost(ctx context.Context, req *pb.UpdatePostRequest) (*pb.UpdatePostResponse, error) {
+	var description, title *string
+	if req.Description != "" {
+		description = &req.Description
+	}
+	if req.Title != "" {
+		title = &req.Title
+	}
+
+	err := p.database.UpdatePost(req.PostId, req.UserId, title, description)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.UpdatePostResponse{
+		Message: "successfully updated post",
+	}, nil
+}
+
+func (p *PostServiceServer) GetPost(ctx context.Context, req *pb.GetPostRequest) (*pb.GetPostResponse, error) {
+	postId, userId, title, description, extension, tagsIds, err := p.database.GetPost(req.PostId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.GetPostResponse{
+		Post: &pb.Post{
+			PostId:      postId + "." + extension,
+			UserId:      userId,
+			Title:       title,
+			Description: *description,
+			TagIds:      tagsIds,
+		},
+	}, nil
+}
+
+func (p *PostServiceServer) GetPosts(ctx context.Context, req *pb.GetPostsRequest) (*pb.GetPostsResponse, error) {
+	postIds, userIds, titles, descriptions, extensions, tagsIds, err := p.database.GetPosts(req.PostIds)
+	if err != nil {
+		return nil, err
+	}
+
+	posts := make([]*pb.Post, len(postIds))
+	for i := range postIds {
+		posts[i] = &pb.Post{
+			PostId:      postIds[i] + "." + extensions[i],
+			UserId:      userIds[i],
+			Title:       titles[i],
+			Description: *descriptions[i],
+			TagIds:      tagsIds[i],
+		}
+	}
+
+	return &pb.GetPostsResponse{
+		Posts: posts,
+	}, nil
+}
+
+func (p *PostServiceServer) SearchPosts(ctx context.Context, req *pb.SearchPostsRequest) (*pb.SearchPostsResponse, error) {
+	var tagIds, userIds []string
+	if req.TagId != nil && len(req.TagId) > 0 {
+		tagIds = req.TagId
+	}
+
+	if req.UserId != nil && len(req.UserId) > 0 {
+		userIds = req.UserId
+	}
+
+	var query *string
+	if req.Query != "" {
+		query = &req.Query
+	}
+
+	postIds, userIdsRes, titles, descriptions, extensions, tagsIds, err := p.database.SearchPosts(query, tagIds, userIds, int(req.Limit), int(req.Offset))
+	if err != nil {
+		return nil, err
+	}
+
+	posts := make([]*pb.Post, len(postIds))
+	for i := range postIds {
+		posts[i] = &pb.Post{
+			PostId:      postIds[i] + "." + extensions[i],
+			UserId:      userIdsRes[i],
+			Title:       titles[i],
+			Description: *descriptions[i],
+			TagIds:      tagsIds[i],
+		}
+	}
+
+	return &pb.SearchPostsResponse{
+		Posts: posts,
+	}, nil
+}
+
+func (p *PostServiceServer) DeletePost(ctx context.Context, req *pb.DeletePostRequest) (*pb.DeletePostResponse, error) {
+	err := p.database.DeletePost(req.PostId, req.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.DeletePostResponse{
+		Message: "successfully deleted post",
+	}, nil
+}
+
+func (p *PostServiceServer) CreateBoard(ctx context.Context, req *pb.CreateBoardRequest) (*pb.CreateBoardResponse, error) {
+	var description *string
+	if req.Description != "" {
+		description = &req.Description
+	}
+
+	boardId, err := p.database.CreateBoard(req.UserId, req.Name, description)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CreateBoardResponse{
+		BoardId: boardId,
+	}, nil
+}
+
+func (p *PostServiceServer) UpdateBoard(ctx context.Context, req *pb.UpdateBoardRequest) (*pb.UpdateBoardResponse, error) {
+	var description, name *string
+	if req.Description != "" {
+		description = &req.Description
+	}
+	if req.Name != "" {
+		name = &req.Name
+	}
+
+	err := p.database.UpdateBoard(req.BoardId, req.UserId, name, description)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.UpdateBoardResponse{
+		Message: "successfully updated board",
+	}, nil
+}
+
+func (p *PostServiceServer) GetBoard(ctx context.Context, req *pb.GetBoardRequest) (*pb.GetBoardResponse, error) {
+	boardId, userId, name, description, pinnedPostIds, err := p.database.GetBoard(req.BoardId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.GetBoardResponse{
+		Board: &pb.Board{
+			BoardId:     boardId,
+			UserId:      userId,
+			Name:        name,
+			Description: *description,
+			PostIds:     pinnedPostIds,
+		},
+	}, nil
+}
+
+func (p *PostServiceServer) GetBoards(ctx context.Context, req *pb.GetBoardsRequest) (*pb.GetBoardsResponse, error) {
+	boardIds, userIds, names, descriptions, pinnedPostIds, err := p.database.GetBoards(req.BoardIds)
+	if err != nil {
+		return nil, err
+	}
+
+	boards := make([]*pb.Board, len(boardIds))
+	for i := range boardIds {
+		boards[i] = &pb.Board{
+			BoardId:     boardIds[i],
+			UserId:      userIds[i],
+			Name:        names[i],
+			Description: *descriptions[i],
+			PostIds:     pinnedPostIds[i],
+		}
+	}
+	return &pb.GetBoardsResponse{
+		Boards: boards,
+	}, nil
+}
+
+func (p *PostServiceServer) SearchBoards(ctx context.Context, req *pb.SearchBoardsRequest) (*pb.SearchBoardsResponse, error) {
+	var query *string
+	if req.Query != "" {
+		query = &req.Query
+	}
+
+	var userIds []string
+	if req.UserId != nil && len(req.UserId) > 0 {
+		userIds = req.UserId
+	}
+
+	boardIds, userIdsRes, names, descriptions, pinnedPostIds, err := p.database.SearchBoards(query, userIds, int(req.Limit), int(req.Offset))
+	if err != nil {
+		return nil, err
+	}
+
+	boards := make([]*pb.Board, len(boardIds))
+	for i := range boardIds {
+		boards[i] = &pb.Board{
+			BoardId:     boardIds[i],
+			UserId:      userIdsRes[i],
+			Name:        names[i],
+			Description: *descriptions[i],
+			PostIds:     pinnedPostIds[i],
+		}
+	}
+
+	return &pb.SearchBoardsResponse{
+		Boards: boards,
+	}, nil
+}
+
+func (p *PostServiceServer) DeleteBoard(ctx context.Context, req *pb.DeleteBoardRequest) (*pb.DeleteBoardResponse, error) {
+	err := p.database.DeleteBoard(req.BoardId, req.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.DeleteBoardResponse{
+		Message: "successfully deleted board",
+	}, nil
+}
+
+func (p *PostServiceServer) CreateTag(ctx context.Context, req *pb.CreateTagRequest) (*pb.CreateTagResponse, error) {
+	tagId, err := p.database.CreateTag(req.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CreateTagResponse{
+		TagId: tagId,
+	}, nil
+}
+
+func (p *PostServiceServer) GetTag(ctx context.Context, req *pb.GetTagRequest) (*pb.GetTagResponse, error) {
+	tagId, name, err := p.database.GetTag(req.TagId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.GetTagResponse{
+		TagId: tagId,
+		Name:  name,
+	}, nil
+
+}
+
+func (p *PostServiceServer) SearchTags(ctx context.Context, req *pb.SearchTagsRequest) (*pb.SearchTagsResponse, error) {
+	var query *string
+	if req.Query != "" {
+		query = &req.Query
+	}
+
+	tagIds, names, err := p.database.SearchTags(query, int(req.Limit), int(req.Offset))
+	if err != nil {
+		return nil, err
+	}
+
+	tags := make([]*pb.Tag, len(tagIds))
+	for i := range tagIds {
+		tags[i] = &pb.Tag{
+			TagId: tagIds[i],
+			Name:  names[i],
+		}
+	}
+
+	return &pb.SearchTagsResponse{
+		Tags: tags,
+	}, nil
+}
+
+func (p *PostServiceServer) PinPostToBoard(ctx context.Context, req *pb.PinPostToBoardRequest) (*pb.PinPostToBoardResponse, error) {
+	err := p.database.PinPostToBoard(req.PostId, req.BoardId, req.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.PinPostToBoardResponse{
+		Message: "successfully pinned post to board",
+	}, nil
+}
+
+func (p *PostServiceServer) UnpinPostFromBoard(ctx context.Context, req *pb.UnpinPostFromBoardRequest) (*pb.UnpinPostFromBoardResponse, error) {
+	err := p.database.UnpinPostFromBoard(req.PostId, req.BoardId, req.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.UnpinPostFromBoardResponse{
+		Message: "successfully unpinned post from board",
+	}, nil
+}
+
+func (p *PostServiceServer) AddTagToPost(ctx context.Context, req *pb.AddTagToPostRequest) (*pb.AddTagToPostResponse, error) {
+	err := p.database.AddTagToPost(req.PostId, req.TagId, req.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.AddTagToPostResponse{
+		Message: "successfully added tag to post",
+	}, nil
+}
+
+func (p *PostServiceServer) RemoveTagFromPost(ctx context.Context, req *pb.RemoveTagFromPostRequest) (*pb.RemoveTagFromPostResponse, error) {
+	err := p.database.RemoveTagFromPost(req.PostId, req.TagId, req.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.RemoveTagFromPostResponse{
+		Message: "successfully removed tag from post",
+	}, nil
 }
